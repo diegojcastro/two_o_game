cb10bca29a636ca8889414f3d68905a668c59a34
Diego Castro <diegojcr4@gmail.com>
2021-10-11 10:08:07 -0400

    first commit

---
+++ game.rb
@@ -0,0 +1,17 @@
+=begin
+Holds the main game loop.
+IV: turn, current_player
+=end
+
+class Game
+  def initialize(turn, players)
+    @turn = turn
+    @players = players
+    @current_player = @players[0]
+  end
+  
+  def next_question
+  
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,6 @@
+=begin
+Player class
+IV: name, lives
+Takes a turn to answer questions, either doing nothing on a
+correct answer or losing a life on an incorrect one.
+=end
